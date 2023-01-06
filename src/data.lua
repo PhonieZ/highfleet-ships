e7a672612b2bd725f30ba691b803105f6e9b820c
Phoniez <66821881+PhonieZ@users.noreply.github.com>
2023-01-06 19:10:35 +0000

    Edited Main Mod File

--- src/data.lua
+++ src/data.lua
@@ -1,2 +1,8 @@
 --Imports
 require("prototypes.fluid")
+require("prototypes.item")
+require("prototypes.recipe")
+require("prototypes.item-groups")
+require("prototypes.entity.entities")
+require("prototypes.categories.recipe-category")
+require("prototypes.lmfao")--lmfao
