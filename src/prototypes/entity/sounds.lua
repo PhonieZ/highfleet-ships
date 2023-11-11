65f9c8cb18e98c725e933f89ef7ebe97a4da7bd7
Phoniez <66821881+PhonieZ@users.noreply.github.com>
2023-11-11 11:32:13 +0000

    Added Sounds File

---
+++ src/prototypes/entity/sounds.lua
@@ -0,0 +1,5 @@
+sounds={}
+
+
+sounds.oil_refinery=data.raw["assembling-machine"]["oil-refinery"].working_sound.sound
+sounds.chemical_plant=data.raw["assembling-machine"]["chemical-plant"].working_sound.sound
